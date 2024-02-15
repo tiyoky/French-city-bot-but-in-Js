1b59eb6abdd10bd9eea85c70c15037c6917560f7
tiyoky <160074145+tiyoky@users.noreply.github.com>
2024-02-15 18:27:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+intents = discord.Intents.default()
+intents.typing = true
+intents.presences = true
+intents.members = true
 
 // Préfixe du bot
 const prefix = '+';
